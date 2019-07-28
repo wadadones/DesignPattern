d19619f2f6d8be371a8efcf632f1917eb6f53ea7
n.wada <jones3429@outlook.jp>
2019-07-28 23:34:43 +0900

    finalize template method

--- TemplateMethod/Display/AbstractDisplay.java
+++ TemplateMethod/Display/AbstractDisplay.java
@@ -2,7 +2,7 @@ abstract class AbstractDisplay {
     abstract void open();
     abstract void print();
     abstract void close();
-    void display() {
+    public final void display() {
         open();
         for(int i = 0; i < 5; i++) {
             print();
